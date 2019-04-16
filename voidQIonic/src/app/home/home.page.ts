af8cc4ed3f85b6e5049d2b6b9fb5ade59dea6a2e
Jitrakorn Tan <jitrakorntan@gmail.com>
2019-04-16 18:49:56 +0800

    changes

--- voidQIonic/src/app/home/home.page.ts
+++ voidQIonic/src/app/home/home.page.ts
@@ -1,217 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  ToastController,
-  Platform
-} from '@ionic/angular';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  Marker,
-  GoogleMapsAnimation,
-  MyLocation
-} from '@ionic-native/google-maps/';
 
-import { ClinicService } from '../clinic.service';
+import { NavController } from '@ionic/angular';
+
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 import { SessionService } from '../session.service';
-import { Clinic } from '../clinic';
+import { PatientService } from '../patient.service';
+import { Patient } from '../patient';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 
-export class HomePage implements OnInit
-{
-	clinics: Clinic[];
-  
-	map: GoogleMap;
-	loading: any;
-  async ngOnInit() {
-    // Since ngOnInit() is executed before `deviceready` event,
-		// you have to wait the event.
-		
-	
+export class HomePage implements OnInit {
 
+	submitted: boolean;
+	username: string;
+	password: string;
+	loginError: boolean;
+	errorMessage: string;
 
-    this.platform.ready();
-    this.loadMap();
-  }
-
-	 
-	constructor(public sessionService: SessionService,public toastCtrl: ToastController,
-		private platform: Platform,private clinicService: ClinicService)
-	{
+	constructor(private router: Router,
+		public sessionService: SessionService,
+		private patientService: PatientService,
+		private navCtrl:NavController) {
+		this.submitted = false;
 	}
-  loadMap() {
-		this.map = GoogleMaps.create('map_canvas', {
-		  // camera: {
-		  //   target: {
-		  //     lat: 43.0741704,
-		  //     lng: -89.3809802
-		  //   },
-		  //   zoom: 18,
-		  //   tilt: 30
-		  // }
-		});
-		this.goToMyLocation();
-	  }
-	
-	
-	  goToMyLocation(){
-		this.map.clear();
 
-
-		this.clinicService.retrieveClinics().subscribe(
-			response => {
-				this.clinics = response.clinicEntities;
-				console.log(this.clinics);
-
-				var clinicLocations = 
-				{
-						"lat": response.clinicEntities.lat,
-						"lng": response.clinicEntities.lng
-					
-				};
-
-				
-
-
-			},
-			error => {
-				console.log('********** homepage.ts: ' + error);
-			}
-		);
-
-
-	// Get the location of you
-	this.map.getMyLocation().then((location: MyLocation) => {
-		console.log(JSON.stringify(location, null ,2));
-
-		// Move the map camera to the location with animation
-		this.map.animateCamera({
-		target: location.latLng,
-		zoom: 14,
-		duration: 1000
-		});
-		
-		this.clinicService.retrieveClinics().subscribe(
-			response => {
-				this.clinics = response.clinicEntities;
-				console.log(this.clinics);
-
-				var clinicLocations = 
-				{
-						"lat": response.clinicEntities.lat,
-						"lng": response.clinicEntities.lng
-					
-				};
-					//add a marker
-		let marker: Marker = this.map.addMarkerSync({
-			title:  response.clinicEntities.clinicName,
-			snippet: '3km away',
-			position: clinicLocations,
-			animation: GoogleMapsAnimation.DROP
-			});
-	
-		
-			//show the infoWindow
-			marker.showInfoWindow();
-		
-
-			},
-			error => {
-				console.log('********** homepage.ts: ' + error);
-			}
-		);
-			
-
-		})
-		.catch(err => {
-		  //this.loading.dismiss();
-		  this.showToast(err.error_message);
-		});
-	  }
-	
-	
-		async showToast(message: string) {
-			let toast = await this.toastCtrl.create({
-				message: message,
-				duration: 2000,
-				position: 'middle'
-			});
-			toast.present();
-			}
-
-
-
-		
-
-			// goToMyLocation(){
-			// 	this.map.clear();
-		
-		
-			// 	this.clinicService.retrieveClinics().subscribe(
-			// 		response => {
-			// 			this.clinics = response.clinicEntities;
-			// 			console.log(this.clinics);
-			// 		},
-			// 		error => {
-			// 			console.log('********** homepage.ts: ' + error);
-			// 		}
-			// 	);
-		
-			
-			// 	// Get the location of you
-			// 	this.map.getMyLocation().then((location: MyLocation) => {
-			// 		console.log(JSON.stringify(location, null ,2));
-			
-			// 		// Move the map camera to the location with animation
-			// 		this.map.animateCamera({
-			// 		target: location.latLng,
-			// 		zoom: 14,
-			// 		duration: 1000
-			// 		});
-			
-			// 		//add a marker
-			// 		let marker: Marker = this.map.addMarkerSync({
-			// 		title: 'HealthWay Clinic!',
-			// 		snippet: '3km away',
-			// 		position: location.latLng,
-			// 		animation: GoogleMapsAnimation.DROP
-			// 		});
-			
-			// 		this.map.
-			// 		//show the infoWindow
-			// 		marker.showInfoWindow();
-			
-				
-			// 		this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
-			// 		(data) => {
-			// 			console.log("Click MAP",data);
-			// 		}
-			// 		);
-			// 	})
-			// 	.catch(err => {
-			// 		//this.loading.dismiss();
-			// 		this.showToast(err.error_message);
-			// 	});
-			// 	}
-			
-			// 	async showToast(message: string) {
-			// 	let toast = await this.toastCtrl.create({
-			// 		message: message,
-			// 		duration: 2000,
-			// 		position: 'middle'
-			// 	});
-			// 	toast.present();
-			// 	}
-			
-	
-
-	
-	 
-	
+	ngOnInit() {
 	}
-	
+
+	patientLogin(patientLoginForm: NgForm) {
+		this.submitted = true;
+		if (patientLoginForm.valid) {
+			this.sessionService.setUsername(this.username);
+			this.sessionService.setPassword(this.password);
+
+			this.patientService.patientLogin(this.username, this.password).subscribe(
+				response => {
+					let patient: Patient = response.patientEntity;
+
+					if (patient != null) {
+						this.sessionService.setIsLogin(true);
+						this.sessionService.setCurrentPatient(patient);
+						this.loginError = false;
+						this.navCtrl.navigateForward('appointments');
+					}
+					else {
+						this.loginError = true;
+					}
+				},
+				error => {
+					this.loginError = true;
+					this.errorMessage = error;
+				}
+			);
+		}
+	}
+}
