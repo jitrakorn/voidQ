b2624454860aa8bc5e92b2cf4bb77fa3373f9db6
Jitrakorn Tan <jitrakorntan@gmail.com>
2019-04-21 16:17:18 +0800

    changes

--- voidQIonic/src/app/patient.service.ts
+++ voidQIonic/src/app/patient.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
-
 import { Patient } from './patient';
 
 const httpOptions2 = {
